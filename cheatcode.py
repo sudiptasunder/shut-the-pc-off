6c9da649c66cee5d43d34f7d2bca8c756e88f27d
Sudipta Sunder <137252282+sudiptasunder@users.noreply.github.com>
2024-05-22 09:02:52 +0530

    Add files via upload

---
+++ cheatcode.py
@@ -0,0 +1,7 @@
+codex_s = "shut-now"
+# The above code shutdowns the PC
+
+codex_r = "mr-reboot"
+# The above code restarts the PC
+
+#Don't try to change the name of the variable, otherwise your cheatcodes may not work properly.
