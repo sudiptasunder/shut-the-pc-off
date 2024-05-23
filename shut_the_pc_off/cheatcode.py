8f2d566794a60d0e284aba8b756de4fe4ec50f9d
Sudipta Sunder <137252282+sudiptasunder@users.noreply.github.com>
2024-05-23 11:23:36 +0530

    Added new cheat codes

--- shut_the_pc_off/cheatcode.py
+++ shut_the_pc_off/cheatcode.py
@@ -4,4 +4,11 @@ codex_s = "shut-now"
 codex_r = "mr-reboot"
 # The above code restarts the PC
 
+# The above code deletes both of the program
+codex_d = "remove-me"
+
+# The below variable is used when the user selects the deletion option
+cheatcode_path = __file__
+
+# *** Important ***
 #Don't try to change the name of the variable, otherwise your cheatcodes may not work properly.
