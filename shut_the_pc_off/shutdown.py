4c7be70c944b104d0a37b5c110402b6bed4210d0
Sudipta Sunder <137252282+sudiptasunder@users.noreply.github.com>
2024-05-25 18:52:16 +0530

    Added new operations, animations, a cheat code and many more!
    1. With this commit, we have added some new cool logo animations to the program. When you will open up the program, you will see the logo in animation. 
    
    2. We have added a operation to directly terminate the application allowing users to use the program once on their demand. By selecting '999' you can perform this operation. You can also use the cheat code 'exit' to exit the application.
    
    3. We also allow our users to turn off the animations. You can type 'False' in the 'animation_switch.py' to turn off the loading animation. 
    
    4. Overall, this is a major update and with this commit, we have improved many things which maybe difficult to notice. We tried to improve user experience with this update. Keep using it and keep sharing your views and feedbacks with us. We will improve our program overtime with feedbacks to provide a good user experience.
    
    Thanks!

--- shut_the_pc_off/shutdown.py
+++ shut_the_pc_off/shutdown.py
@@ -1,5 +1,7 @@
 #Importing the modules
 import os
+import animation
+import animation_switch
 
 #Importing the cheatcode
 import cheatcode
@@ -7,24 +9,26 @@ import cheatcode
 #File Path
 file_path = os.getcwd() 
 
+#Fetching file name
+file_name = os.path.basename(__file__)
+
 #Logo for the program
-print('''
-╔═══╦╗────╔╗──╔╗╔╗─────╔═══╦═══╗────╔═╗╔═╗
-║╔═╗║║───╔╝╚╗╔╝╚╣║─────║╔═╗║╔═╗║────║╔╝║╔╝
-║╚══╣╚═╦╗╠╗╔╝╚╗╔╣╚═╦══╗║╚═╝║║─╚╝╔══╦╝╚╦╝╚╗
-╚══╗║╔╗║║║║║──║║║╔╗║║═╣║╔══╣║─╔╗║╔╗╠╗╔╩╗╔╝
-║╚═╝║║║║╚╝║╚╗─║╚╣║║║║═╣║║──║╚═╝║║╚╝║║║─║║
-╚═══╩╝╚╩══╩═╝─╚═╩╝╚╩══╝╚╝──╚═══╝╚══╝╚╝─╚╝\nDeveloped By Sudipta Sunder''')
-print("Current Directory: ",file_path)
-print("File Path: ", __file__,"\n\n")
+if animation_switch.animation_switch == True:
+    animation.logo_animation()
+if animation_switch.animation_switch == False:
+    animation.logo_only()
+
+# Displays addition details
+print("Current Directory- ",file_path)
+print("File Path- ", __file__)
+print("File Name: ",file_name,"\n\n")
 
 #This variable helps to run the program continiously...
 run = True
 
 #User selection menu
-print("WARNING!\nDON'T TRY TO CHANGE THE NAME OF THIS PROGRAM, OTHERWISE THE PROGRAM WON'T WORK AND WILL STUCK IN A CONTINIOUS LOOP!\n")
-print('1. DIRECT SHUTDOWN (LESS THAN 1 MIN)\n2. DIRECT RESTART (LESS THAN 1 MIN)\n3. TIMED SHUTDOWN\n4. TIMED RESTART\n5. ABORT SHUTDOWN OR RESTART\n6. TYPE "000" FOR IMMEDIATE SHUTDOWN OR RESTART\n7. TYPE "001" TO DELETE THIS PROGRAM\n\nENTER YOUR CHEAT CODE(S) TO SHUTDOWN OR RESTART IN 0S]\n')
-print("\nTYPE LETTER(S) IN UPPER CASE IN NEXT PROCESS ONLY IF GO WITH THE 6TH OPTION\n")
+print('1. DIRECT SHUTDOWN (LESS THAN 1 MIN)\n2. DIRECT RESTART (LESS THAN 1 MIN)\n3. TIMED SHUTDOWN\n4. TIMED RESTART\n5. ABORT SHUTDOWN OR RESTART\n6. TYPE "000" FOR IMMEDIATE SHUTDOWN OR RESTART\n7. TYPE "001" TO DELETE THIS PROGRAM\n8. TYPE "002" TO PERFORM OPERATION(S) ON OTHER COMPUTER(S) CONNECTED TO THE SAME NETWORK\n9. TYPE "003" TO PERFORM OPERATION(S) BY LEAVING A MESSAGE\n10. PRESS "999" TO EXIT\n\n*****ENTER YOUR CHEAT CODE(S) TO SHUTDOWN OR RESTART IN 0S*****\n\n')
+print("\nTYPE LETTER(S) IN UPPER CASE IN NEXT PROCESS ONLY IF GO WITH THE (6,7,8 & 9) OPTION(S)\n")
 user_input = input("SELECT ONE OF THE FOLLOWING OPERATIONS:    \n")
 
 #What will happen if you select 1
@@ -40,13 +44,13 @@ elif user_input == '2':
 #What will happen if you select 3
 elif user_input == '3':
     print("THE COMPUTER WILL SHUTDOWN AS PER THE TIME PROVIDED BY THE USER.\n")
-    time = str(input("ENTER YOUR TIME:\n"))
+    time = str(input("ENTER YOUR TIME(in seconds):\n"))
     os.system(f'cmd /k "shutdown -s -t {time}"')
 
 #What will happen if you select 4
 elif user_input == '4':
     print("THE COMPUTER WILL RESTART AS PER THE TIME PROVIDED BY THE USER.\n")
-    time = str(input("ENTER YOUR TIME:\n"))
+    time = str(input("ENTER YOUR TIME(in seconds):\n"))
     os.system(f'cmd /k "shutdown -r -t {time}"')
 
 #What will happen if you select 5
@@ -85,32 +89,68 @@ elif user_input == '001':
     if confirm_deletion == "YES":
         os.remove(__file__)
         os.remove(cheatcode.cheatcode_path)
+        os.remove(animation.animation_path)
+        os.remove(animation_switch.animation_switch_path)
         run = False
     elif confirm_deletion == "NO":
         print("THE PROCESS WAS NOT EXECUTED...\n")
     else:
         print("TYPE 'YES' OR 'NO' IN UPPER CASE TO EXECUTE THE COMMANDS")
         print('PROCESS 001 HAS BEEN CANCELLED...\n')
-        run = False
 
-#What will happen if you enter your Cheat Code
+#What will happen if you select 002
+#Open ups the GUI shutdown menu
+elif user_input == "002":
+    print("PLEASE WAIT. WE ARE OPENING THE INTERFACE FOR YOU.\n")
+    os.system('cmd /k "shutdown -i"')
+
+#What will happen if you select 003
+#Perform the operations by leaving a message for the user
+elif user_input == "003":
+    print("PLEASE ENTER YOUR MESSAGE BELOW\n")
+    user_msg = str(input("LEAVE A MESSAGE:\n"))
+    operation_time = str(input("ENTER YOUR TIME(in seconds):    "))
+    operation_type = str(input("TYPE '1' FOR SHUTDOWN AND '2' FOR RESTART\n"))
+    if operation_type == "1":
+        os.system(f'cmd /k "shutdown -s -t {operation_time} -c "{user_msg}""')
+    elif operation_time == "2":
+        os.system(f'cmd /k "shutdown -r -t {operation_time} -c "{user_msg}""')
+    else:
+        print("TYPE '1' OR '2' ONLY. OTHER CHARACTERS ARE NOT ALLOWED. THE PROCESS WAS NOT EXECUTED...\n")
+
+#What will happen if you select 999
+#Terminates the application
+elif user_input == "999":
+    print("TERMINATING APPLICATION...\n")
+    run = False
+
+#What will happen if you enter your Cheat Code(s)
+# Shut down the computer
 elif user_input == cheatcode.codex_s:
     os.system('cmd /k "shutdown -s -t 0"')
 
+# Restarts the computer
 elif user_input == cheatcode.codex_r:
     os.system('cmd /k "shutdown -r -t 0"')
 
+# Deletes the entire program
 elif user_input == cheatcode.codex_d:
     run = False
     os.remove(__file__)
     os.remove(cheatcode.cheatcode_path)
+    os.remove(animation.animation_path)
+    os.remove(animation_switch.animation_switch_path)
+
+# Terminates the session and exits
+elif user_input == cheatcode.codex_exit:
+    print("TERMINATING APPLICATION...\n")
+    run = False
 
 #What will happen if you type something which is not in the menu
 else:
     print("\nYOU CAN ONLY CHOOSE THE ABOVE FOLLOWING OPTIONS.\n")
     print("***IN CASE IF THE CHEATCODE(S) ISN'T WORKING, TRY CHECKING THE NAME OF THE VARIABLE OR CHECK IF THE CODEX_S OR CODEX_R HAS BEEN CHANGED OR NOT IN 'CHEATCODE.PY'***")
-    run = False
 
 #This will keep the program running...Stop it using CTRL + C
 while run:
-    os.system('shutdown.py')
+    os.system(file_name)
